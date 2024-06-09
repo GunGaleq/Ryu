0c0fb1d341cd3cb0b735730911a32bb4afba3a3a
GunGaleq <1203376629@qq.com>
2024-06-09 18:16:35 +0800

    Create Gnutella.py

---
+++ Gnutella.py
@@ -0,0 +1,67 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+import hashlib
+
+class FloodingController(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(FloodingController, self).__init__(*args, **kwargs)
+        self.packet_records = {}  # 用于记录每个数据包的转发记录
+
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+        # 使用哈希值作为唯一标识符
+        packet_id = hashlib.sha256(f"{datapath.id}-{msg.data}".encode('utf-8')).hexdigest()
+            
+        if packet_id in self.packet_records:
+            return  # 不再转发该数据包
+        self.packet_records[packet_id] = []  # 添加记录到字典中
+        
+        # 执行洪范算法，将数据包输出到所有端口（除了输入端口）
+        actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD), parser.OFPActionDecNwTtl()]
+        data = msg.data
+        match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+        self.add_flow(datapath, 1, match, actions)
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
